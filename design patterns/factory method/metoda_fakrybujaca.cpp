ef2f8024135d38bff8d0bec0b7b1e772ac0d105b
Mateusz <mateusz.celmer@gmail.com>
2019-02-27 11:50:18 +0100

    design patterns - factory method

---
+++ design patterns/factory method/metoda_fakrybujaca.cpp
@@ -0,0 +1,46 @@
+/* acknowledgements to: https://www.p-programowanie.pl/wzorce-projektowe/fabryka-abstrakcyjna/#more-3197 */
+
+#include <iostream>
+
+using namespace std;
+
+class IShape
+{
+};
+
+class Square : public IShape
+{
+};
+class Triangle : public IShape
+{
+};
+
+class ShapeFactory
+{
+  public:
+    virtual IShape *createShape() = 0;
+};
+
+class SquareFactory : public ShapeFactory
+{
+    IShape *createShape()
+    {
+        return new Square();
+    }
+};
+
+class TriangleFactory : public ShapeFactory
+{
+    IShape *createShape()
+    {
+        return new Triangle();
+    }
+};
+
+int main()
+{
+    ShapeFactory *shapeFactory = new SquareFactory();
+
+    IShape *square = (*shapeFactory).createShape();
+    return 0;
+}
