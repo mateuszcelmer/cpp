87187e855f9b7e74302f3ae8e59abc25d2651fca
Mateusz <mateusz.celmer@gmail.com>
2019-03-03 18:31:27 +0100

    move and swap

--- move semantics/move.cpp
+++ move semantics/move.cpp
@@ -23,23 +23,39 @@ class Derived : public Base
 
 int main()
 {
-    vector<unique_ptr<Base>> v1, v2;
-    Derived some("one");
-    v1.push_back(unique_ptr<Derived>(&some));
+    vector<unique_ptr<Base>> v1, v2, v2a;
+    v1.push_back(make_unique<Derived>("one"));
     v2.push_back(make_unique<Derived>("two"));
 
+    cout << "1." << endl;
     cout << v1[0]->get_name() << endl;
     cout << v2[0]->get_name() << endl;
 
     std::swap(v1, v2);
 
+    cout << "2." << endl;
     cout << v1[0]->get_name() << endl;
     cout << v2[0]->get_name() << endl;
 
     std::swap(v1, v2);
 
+    cout << "3." << endl;
     cout << v1[0]->get_name() << endl;
     cout << v2[0]->get_name() << endl;
+
+    v2a = move(v1);
+
+    cout << "4." << endl;
+    cout << "v2a[0]->get_name() " << v2a[0]->get_name() << endl;
+    cout << "v1.size() " << v1.size() << endl;
+    cout << "v2a.size() " << v2a.size() << endl;
+
+    //================================
+
+    vector<unique_ptr<Base>> v3, v4;
+    v3.push_back(make_unique<Derived>());
+    // v4 = v3; // compile time error - This is not a copyable type.
+    v4 = move(v3);
 
     return 0;
 }
