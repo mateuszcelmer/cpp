5d5f481ad04daa137be9d16087afaf655c8279a2
Mateusz <mateusz.celmer@gmail.com>
2019-04-10 19:28:41 +0200

    update

---
+++ STL/algorithms/sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 5, 8, 9, 6, 7, 3, 4, 2, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    sort(arr, arr + n);
+
+    cout << "Array after sorting : \n";
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+
+    return 0;
+}
