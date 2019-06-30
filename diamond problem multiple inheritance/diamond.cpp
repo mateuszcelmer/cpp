0611c01f5b9ebf9d2a798b7ac31e3dc6080659a8
Mateusz <mateusz.celmer@gmail.com>
2019-06-30 13:03:00 +0200

    diamond problem

--- diamond problem multiple inheritance/diamond.cpp
+++ diamond problem multiple inheritance/diamond.cpp
@@ -10,12 +10,8 @@ protected:
 public:
   A()
   {
+    x = 1;
     cout << "Konstruktor A() wywolany" << endl;
-  }
-  A(int i)
-  {
-    x = i;
-    cout << "Konstruktor A(int i) wywolany, x = " << x << endl;
   }
   ~A() { cout << "Destruktor A() wywolany" << endl; }
   void print() { cout << x << endl; }
@@ -23,28 +19,22 @@ public:
 
 /* Poziom 2*/
 
-class B : virtual public A
+class B : public A
 {
 public:
-  B() : A(20) // A(20) konstruktor virtualnej klasy, znajdujący się w initializer list, jest ignorowany -> https://en.cppreference.com/w/cpp/language/initializer_list
+  B()
   {
     cout << "Konstruktor B() wywolany" << endl;
-  }
-  B(int i)
-  {
-    x = i;
-    cout << "Konstruktor B(int i) wywolany, x = " << x << endl;
   }
   ~B() { cout << "Destruktor B() wywolany" << endl; }
 };
 
 /* Poziom 2*/
-class C : virtual public A
+class C : public A
 {
 public:
   C()
   {
-    A(30);
     cout << "Konstruktor C() wywolany" << endl;
   }
   ~C() { cout << "Destruktor C() wywolany" << endl; }
@@ -54,7 +44,7 @@ public:
 class D : public B, public C
 {
 public:
-  D() : B(123), C() { cout << "Konstruktor D() wywolany" << endl; }
+  D() : B(), C() { cout << "Konstruktor D() wywolany" << endl; }
   ~D() { cout << "Destruktor D() wywolany" << endl; }
 };
 
