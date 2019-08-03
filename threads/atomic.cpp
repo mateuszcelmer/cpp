1922b9895de4997e740b3a1e977349d2fd2d8caf
Mateusz <mateusz.celmer@gmail.com>
2019-08-03 11:21:42 +0200

    update

--- threads/atomic.cpp
+++ threads/atomic.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <string>
-#include <chrono>
-#include <thread>
+#include <atomic>
+#include <future>
 
 using namespace std;
 
-// TODO
-
 int main()
 {
+    std::atomic<int> counter{100};
+
+    auto f = std::async(std::launch::deferred, [&]() { counter = {111}; });
+    std::cout << counter.load() << std::endl;
+
+    f.get();
+    std::cout << counter.load() << std::endl;
+
+    counter++;
+    std::cout << counter.load() << std::endl;
+
+    counter = 123;
+    std::cout << counter.load() << std::endl;
+
     return 0;
 }
